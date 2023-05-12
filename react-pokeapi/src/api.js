4e6b8517ef8883cffdaeb91975d0356365c3ce4b
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-05-13 00:34:23 +0300

    new api call for searching pokemon by name

--- react-pokeapi/src/api.js
+++ react-pokeapi/src/api.js
@@ -19,3 +19,5 @@ export const generationURL = () => `${base_url}generation/`;
 export const selectedGenerationURL = (id) => `${base_url}generation/${id}`;
 //Pokemon
 export const pokemonURL = () => `${base_url}pokemon/?offset=0&limit=6`;
+//Search Pokemon by name
+export const searchURL = (search) => `${base_url}pokemon/${search}`;
