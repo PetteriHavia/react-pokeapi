df1c7f199f275167fb2669c2dc1a8b3bdd8df1e7
Petteri Havia <petsku15@hotmail.fi>
2023-05-13 00:39:27 +0300

    display searched pokemons and added conditional checks

--- react-pokeapi/src/pages/Home.js
+++ react-pokeapi/src/pages/Home.js
@@ -9,10 +9,10 @@ import Navigation from "../components/Navigation";
 import Pokemon from "../components/Pokemon";
 
 const Home = () => {
-  const [pokemonUrl, setPokemonUrl] = useState([]);
   const [randomData, setRandomData] = useState([]);
   const [generation, setGeneration] = useState([]);
   const [pokemonData, setPokemonData] = useState([]);
+  const [searched, setSearched] = useState([]);
 
   const fetchData = async () => {
     //Fetch Axios
@@ -21,7 +21,7 @@ const Home = () => {
     const pokemonData = await axios.get(pokemonURL());
 
     //setPokemonUrl(randomPokemon.data.results);
-    getRandomPokemon(randomPokemon.data.results, );
+    //getRandomPokemon(randomPokemon.data.results);
     getGeneration(generationCall.data.results);
     getPokemon(pokemonData.data.results);
   };
@@ -52,23 +52,55 @@ const Home = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [setPokemonData, setSearched]);
 
   return (
     <div>
       <Navigation />
       <Container>
         {/*<SimpleSlider randomPokemon={randomData}/>*/}
-        <Search generation={generation} setGeneration={setGeneration} pokemon={pokemonData} setPokemonData={setPokemonData}/>
+        <Search
+          generation={generation}
+          setGeneration={setGeneration}
+          pokemon={pokemonData}
+          setPokemonData={setPokemonData}
+          searched={searched}
+          setSearched={setSearched}
+        />
+        {searched.length ? (
+          <>
+            <h2>Search Results</h2>
+            <PokemonGrid>
+              {searched.slice().reverse().map((pokemon) => (
+                <Pokemon
+                  data={pokemon}
+                  setPokemonData={setPokemonData}
+                  key={pokemon.id}
+                  type={pokemon.types[0].type.name}
+                  pokemonData={pokemonData}
+                />
+              ))}
+            </PokemonGrid>
+          </>
+        ) : (
+          ""
+        )}
         <PokemonGrid>
-          {pokemonData.map((pokemon) => (
-            <Pokemon
-              data={pokemon}
-              setPokemonData={setPokemonData}
-              key={pokemon.id}
-              type={pokemon.types[0].type.name}
-            />
-          ))}
+          {pokemonData.length > 0 ? (
+            pokemonData.map((pokemon) => (
+              <Pokemon
+                data={pokemon}
+                setPokemonData={setPokemonData}
+                key={pokemon.id}
+                type={pokemon.types[0].type.name}
+                pokemonData={pokemonData}
+              />
+            ))
+          ) : (
+            <Loading>
+              <p>Loading Data...</p>
+            </Loading>
+          )}
         </PokemonGrid>
       </Container>
     </div>
@@ -93,6 +125,11 @@ const PokemonGrid = styled.div`
   ); //repeat 350 minimun space for column, if not enough space then take rest of the space with 1fr
   grid-column-gap: 3rem;
   grid-row-gap: 5rem;
+`;
+
+const Loading = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 export default Home;
