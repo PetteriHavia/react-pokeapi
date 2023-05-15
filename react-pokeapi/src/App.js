792f0ef6c9f5fee9e869c339ec6f7c02a48fd144
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-05-15 03:33:55 +0300

    started react-router-dom

--- react-pokeapi/src/App.js
+++ react-pokeapi/src/App.js
@@ -1,12 +1,21 @@
 import GlobalStyles from "./components/GlobalStyles";
+import PokemonDetail from "./components/PokemonDetails";
 import Home from "./pages/Home";
+import PokemonDetails from "./components/PokemonDetails";
+import Navigation from "./components/Navigation";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
         <GlobalStyles />
-        <Home />
+        <Navigation />
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" exact element={<Home />} />
+          <Route path="pokemons/:id" element={<PokemonDetails />} />
+        </Routes>
     </div>
   );
 }
