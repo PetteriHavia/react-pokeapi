4b2f2fbab6357817bfabfb84f44ed0327f742d13
Petteri Havia <petsku15@hotmail.fi>
2023-05-23 02:19:19 +0300

    update + clean up code

--- react-pokeapi/src/components/Pokemon.js
+++ react-pokeapi/src/components/Pokemon.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import GlobalStyles from "./GlobalStyles";
-import {Link, useParams} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 
-const Pokemon = ({ data, setPokemonData, type }) => {
+const Pokemon = ({ data, type }) => {
   
   const PokemonCard = ({ data }) => {
     return (
