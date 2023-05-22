c98ec6e434c4c1d397e5c203aed094ac4fa47a0e
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-05-23 02:18:22 +0300

    Add PokemonStats + clean up the code

--- react-pokeapi/src/components/PokemonDetails.js
+++ react-pokeapi/src/components/PokemonDetails.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { pokemonDetails } from "../api";
+import { pokemonDetails, searchURL } from "../api";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import PokemonBio from "./PokemonBio";
+import PokemonStats from "./PokemonStats";
 
 const PokemonDetails = () => {
   const [pokemon, setPokemon] = useState(null);
@@ -12,39 +13,50 @@ const PokemonDetails = () => {
   const { singleId } = useParams();
 
   const getSpecies = async (pokemon) => {
-    try{
+    try {
       const speciesData = await axios.get(pokemon.species.url);
       setSpecies(speciesData.data);
-    }catch (error){
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try{
-            const pokemonData = await axios.get(pokemonDetails(singleId));
-            setPokemon(pokemonData.data);
-            getSpecies(pokemonData.data);
-        }catch(error){
-            console.log(error);
-        }
-      };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const pokemonData = await axios.get(searchURL(singleId));
+        setPokemon(pokemonData.data);
+        getSpecies(pokemonData.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-      fetchData();
-    },[singleId, species])
+    fetchData();
+  }, [singleId, species]);
 
   return (
     <>
-    {pokemon ? (
-    <PokemonBio pokemon={pokemon} type={pokemon.types[0].type.name} species={species}/>
-    ) : (
+      {pokemon ? (
+        <Content>
+          <PokemonBio
+            pokemon={pokemon}
+            type={pokemon.types[0].type.name}
+            species={species}
+          />
+          <PokemonStats pokemon={pokemon} species={species} />
+        </Content>
+      ) : (
         <div>
-            <p>Loading Data...</p>
+          <p>Loading Data...</p>
         </div>
-    )}
+      )}
     </>
   );
 };
+
+const Content = styled.div`
+  padding: 0rem 16rem;
+`;
 
 export default PokemonDetails;
